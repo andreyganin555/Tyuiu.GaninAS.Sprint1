0889581d6e3de3c5ccac8f72e49fdbafd806d37a
AndreyGanin <ganin25@inbox.ru>
2025-09-23 18:26:56 +0500

    Add project files.

---
+++ Tyuiu.GaninAS.Sprint1.Task0.V12.Lib/DataService.cs
@@ -0,0 +1,10 @@
+﻿namespace Tyuiu.GaninAS.Sprint1.Task0.V12.Lib
+{
+    public class DataService
+    {
+        public double Calculate()
+        {
+            return (5 * 2 - 2) / 4 * 3;
+        }
+    }
+} 
---
+++ Tyuiu.GaninAS.Sprint1.Task0.V12.Test/DataServiceTest.cs
@@ -0,0 +1,16 @@
+﻿using Tyuiu.GaninAS.Sprint1.Task0.V12.Lib;
+
+namespace Tyuiu.GaninAS.Sprint1.Task0.V12.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            var res = ds.Calculate();
+            Assert.AreEqual(6, res);
+        }
+    }
+}
