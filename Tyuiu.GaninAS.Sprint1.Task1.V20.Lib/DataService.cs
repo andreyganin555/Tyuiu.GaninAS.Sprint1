9bf2056fbbe458e51d85392c3eaf81ca85399bb8
AndreyGanin <ganin25@inbox.ru>
2025-09-26 16:14:16 +0500

    Task1

---
+++ Tyuiu.GaninAS.Sprint1.Task1.V20.Lib/DataService.cs
@@ -0,0 +1,12 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint1;
+
+namespace Tyuiu.GaninAS.Sprint1.Task1.V20.Lib
+{
+    public class DataService : ISprint1Task1V20
+    {
+        public double Calculate(double x, double y)
+        {
+            return (x * y / 2) + 10;
+        }
+    }
+}
