f65cbb0293b49fcd61aba30917c80b80eeceb427
AndreyGanin <ganin25@inbox.ru>
2025-10-07 14:43:06 +0500

    Task7

---
+++ Tyuiu.GaninAS.Sprint1.Task7.V28.Lib/DataService.cs
@@ -0,0 +1,11 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint1;
+namespace Tyuiu.GaninAS.Sprint1.Task7.V28.Lib;
+
+public class DataService : ISprint1Task7V28
+{
+    public double Calculate(double x, double y)
+    {
+        double res = Math.Pow(Math.Cos(x), 2) * (Math.Sin(1 / x) + Math.Pow(1, 2));
+        return Math.Round(res, 3);
+    }
+}
